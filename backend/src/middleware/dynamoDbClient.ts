dc1b64c518dbb05acd935e32a2d5548452d19134
MnqobiNkabinde <mnqobi.nkabinde@bbd.co.za>
2025-08-07 17:57:20 +0200

    Removed session credentials

--- backend/src/middleware/dynamoDbClient.ts
+++ backend/src/middleware/dynamoDbClient.ts
@@ -4,12 +4,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export function getDynamoClient() {
-  return new DynamoDBClient({
-      credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
-        sessionToken: process.env.AWS_SESSION_TOKEN || "",
-      },
-      region: process.env.AWS_REGION || "af-south-1"
-    });
+  return new DynamoDBClient();
 }
