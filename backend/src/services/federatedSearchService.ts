07f889fbd25c1adc26cb7491d4fce60b70d84143
Thabang Modisaotsile <thabang.modisaotsile@standardbank.co.za>
2025-08-07 22:50:27 +0200

    fediverse workflow

--- backend/src/services/federatedSearchService.ts
+++ backend/src/services/federatedSearchService.ts
@@ -1,6 +1,5 @@
 import { User } from "../types/user";
 import { requestFediverseServer } from "../utils/fediverse-service";
-import { User } from "../types/user";
 
 export const searchFederatedUsers = async (query: string): Promise<User[]> => {
   try {
